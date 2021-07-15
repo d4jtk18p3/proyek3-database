b8d9b9c0f915946b54bdba30bed71c5364c52a46
M.Andhika <48423397+andhika276@users.noreply.github.com>
2021-07-16 00:58:34 +0700

    Update 09-membuat-indeks_prestasi.cjs

--- resources/migrations/09-membuat-indeks_prestasi.cjs
+++ resources/migrations/09-membuat-indeks_prestasi.cjs
@@ -3,10 +3,30 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('Indeks_prestasi', {
-      ip_semester: {
+      id_ip: {
         type: Sequelize.STRING,
         allowNull: false,
         primaryKey:true,
+      },	
+      ip_semester: {
+        type: Sequelize.DOUBLE,
+        allowNull: false,
+      },
+      nim: {
+		type: Sequelize.STRING,
+        references:{
+          model:'Mahasiswa',
+          key:'nim',
+        },
+        onUpdate: 'cascade',
+        onDelete: 'set null'      
+      },
+	  semester: {
+        type: Sequelize.STRING,
+        references:{
+          model:'Mata_Kuliah',
+          key:'semester',
+        }
       }
     });
   },
